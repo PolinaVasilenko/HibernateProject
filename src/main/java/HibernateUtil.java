fe611655c648112af99b114763e26899ee2cbf4c
polina <polindrom@memos.tk>
2022-09-13 16:42:28 +0300

    add session, work right

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,37 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.io.File;
+
+
+public class HibernateUtil {
+
+
+    private static final SessionFactory sessionFactory = initSessionFactory();
+
+
+    private static SessionFactory initSessionFactory() {
+        try {
+            return new Configuration().configure(new File("src\\main\\resources\\hibernate.cfg.xml")).buildSessionFactory();
+        }
+        catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+
+    public static SessionFactory getSessionFactory() {
+
+        if (sessionFactory == null){
+            initSessionFactory();
+        }
+
+        return sessionFactory;
+    }
+
+
+    public static void close() {
+        getSessionFactory().close();
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.close();
+        HibernateUtil.close();
+
+    }
+}
