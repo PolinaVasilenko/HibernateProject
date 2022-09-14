6053f4bc0bfdb645d7fe2b7c8c40d2ae6a2833c2
polina <polindrom@memos.tk>
2022-09-14 14:35:52 +0300

    add lombok

--- src/main/java/com/hibernate/entity/UserData.java
+++ src/main/java/com/hibernate/entity/UserData.java
@@ -1,69 +1,35 @@
 package com.hibernate.entity;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "user_data", schema = "todolist", catalog = "postgres")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserData {
-    private String email;
-    private String userpassword;
-    private String username;
+
+    @Id
+    @Column(name = "id", nullable = false)
     private Long id;
 
     @Basic
     @Column(name = "email", nullable = false, length = -1)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String email;
 
     @Basic
     @Column(name = "userpassword", nullable = false, length = -1)
-    public String getUserpassword() {
-        return userpassword;
-    }
-
-    public void setUserpassword(String userpassword) {
-        this.userpassword = userpassword;
-    }
+    private String userpassword;
 
     @Basic
     @Column(name = "username", nullable = false, length = -1)
-    public String getUsername() {
-        return username;
-    }
+    private String username;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Id
-    @Column(name = "id", nullable = false)
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserData userData = (UserData) o;
-        return Objects.equals(email, userData.email) &&
-                Objects.equals(userpassword, userData.userpassword) &&
-                Objects.equals(username, userData.username) &&
-                Objects.equals(id, userData.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(email, userpassword, username, id);
-    }
 }
--- src/main/java/com/hibernate/entity/UserRole.java
+++ src/main/java/com/hibernate/entity/UserRole.java
@@ -1,36 +1,38 @@
 package com.hibernate.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Objects;
 
 @Entity
 @Table(name = "user_role", schema = "todolist", catalog = "postgres")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRole {
-    private Long id;
+
 
     @Id
     @Column(name = "id", nullable = false)
-    public Long getId() {
-        return id;
-    }
+    private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserRole userRole = (UserRole) o;
-        return Objects.equals(id, userRole.id);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        UserRole userRole = (UserRole) o;
+//        return Objects.equals(id, userRole.id);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
 }
