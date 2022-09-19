9fdb0799c2856b61856d41cd11262bea03851f5d
polina <polindrom@memos.tk>
2022-09-19 18:37:41 +0300

    add lombok log4j

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -2,18 +2,22 @@ package com.hibernate;
 
 import com.hibernate.HibernateUtil;
 import com.hibernate.entity.User;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.Session;
 
+@Log4j2
 public class Main {
     public static void main(String[] args) {
+
+        log.info("Hibernate tutorial");
         Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.getTransaction().begin();
 
         User user = new User();
-        user.setEmail("kk@mail.com");
-        user.setUsername("Pole");
-        user.setPassword("p1311");
+        user.setEmail("nic7@mail.com");
+        user.setUsername("Alex");
+        user.setPassword("n114343567888");
 
         session.save(user);
         session.getTransaction().commit();
